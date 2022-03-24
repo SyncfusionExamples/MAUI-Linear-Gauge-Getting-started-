80cc4b1d6f07310eab3c2bb437209a549b28484f
Muneesh Kumar <muneeshg@syncfusion.com>
2022-03-24 17:58:16 +0530

    Sample added

---
+++ LinearGaugeGettingStarted/LinearGaugeGettingStarted/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LinearGaugeGettingStarted;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
